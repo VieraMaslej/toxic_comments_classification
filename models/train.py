6aafcc0ea302a978d7579278f498b880f3014b7a
VieraMaslej <51993674+VieraMaslej@users.noreply.github.com>
2020-10-18 11:15:27 +0200

    Update train.py

--- models/train.py
+++ models/train.py
@@ -16,22 +16,35 @@ from sklearn.model_selection import train_test_split
 from keras.optimizers import Adam
 from keras.models import model_from_json
 
-embed_size = 300 
+embed_size = 300
 max_features = 20000 
 maxlen = 200 
 
-# load data
+print('Loading data...')
 train = pd.read_csv(r"train.csv")
-EMBEDDING_FILE = f'glove.840B.300d.txt' # GloVe CC 300D
+test = pd.read_csv(r"test.csv")
+test_labels = pd.read_csv(r"test_labels.csv")
+
+EMBEDDING_FILE = f'glove.840B.300d.txt' 
+
+test = pd.concat([test, test_labels], axis=1)
+test = test[test['toxic']!=-1]
 
 classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-y = train[classes].values
+y_train = train[classes].values
+y_test = test[classes].values
 
-train_sentences = train["comment_text"].fillna("fillna").str.lower()
+train_sentences = train["comment_text"].fillna("fillna")
+test_sentences = test["comment_text"].fillna("fillna")
+
 tokenizer = Tokenizer(num_words=max_features)
 tokenizer.fit_on_texts(list(train_sentences))
+
 tokenized_train_sentences = tokenizer.texts_to_sequences(train_sentences)
+tokenized_test_sentences = tokenizer.texts_to_sequences(test_sentences)
+
 train_padding = pad_sequences(tokenized_train_sentences, maxlen)
+test_padding = pad_sequences(tokenized_test_sentences, maxlen)
 
 def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
 embeddings_index = dict(get_coefs(*o.rstrip().rsplit(' ')) for o in open(EMBEDDING_FILE, encoding='utf8'))
@@ -49,44 +62,59 @@ for word, i in word_index.items():
 # cross validation        
 kfold = KFold(n_splits=10)
 cvscores = []
+accscores = []
+rocscorec = []
 
-for train, val in kfold.split(train_padding, y):
+for train, test in kfold.split(train_padding, y):
     
 #####################################################################################
 #                                 FFNN                                              #
 #####################################################################################
 
-  inputs = Input(shape=(maxlen,))
-  x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inputs)
-  x = Flatten()(x)
-  x = Dense(32, activation='relu')(x)
-  x = Dense(64, activation='relu')(x)
-  x = Dense(128, activation='relu')(x)
-  x = Dropout(0.2)(x)
-  x = Dense(256, activation='relu')(x)
-  output = Dense(6, activation='sigmoid')(x)
-  model = Model(inputs=inputs, outputs=output)
+    inputs = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inputs)
+    x = Flatten()(x)
+    x = Dense(32, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(256, activation='relu')(x)
+    output = Dense(6, activation='sigmoid')(x)
+    model = Model(inputs=inputs, outputs=output)
 
 ################################################################################
 
-  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-  print(model.summary())
+    saved_model = "model.hdf5"
+    checkpoint = ModelCheckpoint(saved_model, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 
-  saved_model = "model.hdf5"
-  checkpoint = ModelCheckpoint(saved_model, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    print('Training model...')
+    history = model.fit(train_padding, y_train, batch_size=32, epochs=5, callbacks=[checkpoint], validation_split=0.1)
 
-  # training
-  history = model.fit(train_padding[train], y[train], batch_size=32, epochs=5, callbacks=[checkpoint], validation_data = train_padding[val], y[val])
-  
-  # load model
-  model = load_model('model.hdf5')
-  # evalute on validation set
-  scores = model.evaluate(train_padding[val], y[val])
+    scores = model.evaluate(train_padding[test], y[test])
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    cvscores.append(scores[1] * 100)
+
+    print("Loading model....")
+    model = load_model('model.hdf5')
+    y_pred = model.predict(test_padding)
+
+    y_int = np.zeros_like(y_pred)
+    y_int[y_pred > 0.5] = 1
+
+    accuracy = accuracy_score(y_test,y_int)
+    print('Accuracy is {}'.format(accuracy))
+    accscores.append(accuracy)
     
-  print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-  cvscores.append(scores[1] * 100)
-
+    rocauc = roc_auc_score(y_test, y_pred)
+    print('Roc-auc score is {}'.format(rocauc))
+    rocscore.append(rocauc)
+    
+    print('Classification report {}'.format(classification_report(y_test, y_int, zero_division=0)))
+    print('Confusion matrix {}'.format(multilabel_confusion_matrix(y_test, y_int)))
+        
 print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+print("Test accuracy is: {} %.2f (+/- %.2f)" %  (np.mean(accscores), np.std(accscores)))
+print("Test roc-auc is: {} %.2f (+/- %.2f)" % (np.mean(rocscores), np.std(rocscores)))
 
 
 #####################################################################################
